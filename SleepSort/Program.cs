63506bef13b1dedfc6d4b6e405f04d2f9860b7a4
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-10 08:13:09 -0700

    Add project files.

---
+++ SleepSort/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+
+WriteLine("Welcome To Sleep Sort");
+List<int> numbers = new();
+
+while (true)
+{
+    Write("Enter Number To Add List : ");
+    var numberStr = ReadLine();
+    if (numberStr == "fin")
+        break;
+    if (int.TryParse(numberStr, out int parsed))
+        numbers.Add(parsed);
+    else WriteLine($"Exception to convert : {numberStr}");
+}
+
+WriteLine("Start Sorting");
+
+
+foreach (var n in numbers)
+    Task.Run(() =>
+    {
+        Thread.Sleep(n);
+        Console.WriteLine(n);
+    });
+
+
+ReadKey();
